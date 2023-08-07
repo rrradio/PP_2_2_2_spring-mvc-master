ae365cd3dea6537055cc3f8a7e3d1630d7fc182d
rrradio <rrrrrradiovolna@yandex.ru>
2023-08-07 17:35:04 +0300

    Initial commit

--- src/main/java/web/dao/CarDAO.java
+++
@@ -1,8 +0,0 @@
-package web.dao;
-
-import web.model.Car;
-import java.util.List;
-
-public interface CarDAO {
-     List<Car> getCarsList(int count);
-}
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,30 +1,47 @@
 package web.service;
 
+
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAOImpl;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    private final CarDAOImpl carDAOImpl;
+    private final List<Car> cars;
 
-    public CarServiceImpl(CarDAOImpl carDAOImpl) {
-        this.carDAOImpl = carDAOImpl;
+    {
+        cars = new ArrayList<>();
+
+        Car Toyota = new Car("Toyota", 111, 25000);
+        Car Lada = new Car("Лада", 222, 45000);
+        Car Volvo = new Car("Volvo", 333, 11000);
+        Car Nissan = new Car("Nissan", 444, 74000);
+        Car Volga = new Car("Волга", 555, 112000);
+
+        cars.add(Toyota);
+        cars.add(Lada);
+        cars.add(Volvo);
+        cars.add(Nissan);
+        cars.add(Volga);
     }
+
 
     @Override
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        List<Car> cars;
+        ArrayList<Car> carList = new ArrayList<>();
         if (count > 5) {
             count = 5;
         }
-        cars = carDAOImpl.getCarsList(count);
-        model.addAttribute("cars", cars);
+        for (int i=0; i<count; i++) {
+            carList.add(cars.get(i));
+        }
+
+        model.addAttribute("cars", carList);
         return "cars";
     }
 }
